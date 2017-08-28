7d3d0cd6c7520b448903832f7bc4f40bb16094c6
Bruno Faria <brunocf@brunos-mbp.br.ibm.com>
2017-08-28 13:43:31 -0300

    Some code organization

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-
-# coding: utf-8
-
-# # Imports
-
-
-
 import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
 from PIL import Image
 
-
-
-# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
+from object_detection.utils import label_map_util
 
-
-
-
-from utils import label_map_util
-
-from utils import visualization_utils as vis_util
-
-
-# # Model preparation
-
-
-# What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
+# This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
@@ -46,59 +17,38 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-
-# ## Download Model
-
-
-'''
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-  file_name = os.path.basename(file.name)
-  if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
-'''
-
-
-
 detection_graph = tf.Graph()
+
 
 def load_to_memory():
     global loaded
     global detection_graph
-    #detection_graph = tf.Graph()
+
     with detection_graph.as_default():
-      od_graph_def = tf.GraphDef()
-      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
     loaded = True
 
-
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-#print(label_map)
+
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-#print(categories)
+
 category_index = label_map_util.create_category_index(categories)
 
 
-
-
 def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
 
 
 IMAGE_SIZE = (20, 16)
 
-
-
 loaded = False
+
 
 def detect_objects(image, threshold):
     global detection_graph
@@ -108,40 +58,35 @@ def detect_objects(image, threshold):
         load_to_memory()
         print(loaded)
 
-    PATH_TO_TEST_IMAGES_DIR2 = 'test_images'
-    TEST_IMAGE_PATH = os.path.join(PATH_TO_TEST_IMAGES_DIR2, '{}'.format(image))
-    #print(TEST_IMAGE_PATH)
+    image_directory = 'test_images'
+    image_path = os.path.join(image_directory, '{}'.format(image))
+
     with detection_graph.as_default():
-      with tf.Session(graph=detection_graph) as sess:
-          image_path = TEST_IMAGE_PATH
-          #print(image_path)
-          image = Image.open(image_path)
-          image_np = load_image_into_numpy_array(image)
+        with tf.Session(graph=detection_graph) as sess:
+            image_path = image_path
+            image = Image.open(image_path)
+            image_np = load_image_into_numpy_array(image)
 
-          image_np_expanded = np.expand_dims(image_np, axis=0)
-          image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
-          boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-          scores = detection_graph.get_tensor_by_name('detection_scores:0')
-          classes = detection_graph.get_tensor_by_name('detection_classes:0')
-          num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-          # Actual detection.
-          (boxes, scores, classes, num_detections) = sess.run(
-              [boxes, scores, classes, num_detections],
-              feed_dict={image_tensor: image_np_expanded})
-          obj_above_thres = 0
-          objs_found = []
-          for i in scores[0]:
-            if i >= (threshold):
-              print(category_index[classes[0][obj_above_thres]])
-              #objs_found.append("{\"name\": \"" + category_index[classes[0][obj_above_thres]]['name'] + "\",\"confidence\": \"" + str(i) + "\"}")
-              objs_found.append({'name': category_index[classes[0][obj_above_thres]]['name'],'confidence': str(i) })
-              obj_above_thres = obj_above_thres + 1
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            obj_above_thres = 0
+            objs_found = []
+            for i in scores[0]:
+                if i >= threshold:
+                    print(category_index[classes[0][obj_above_thres]])
+                    objs_found.append({'name': category_index[classes[0][obj_above_thres]]['name'],'confidence': str(i) })
+                    obj_above_thres = obj_above_thres + 1
 
-          return objs_found
+            return objs_found
 
-          #print("Num Objs: {}".format(obj_above_thres))
-          #print("Objs Found: {}".format(objs))
-
-#print(detect_objects('image1.jpg', 0.50))
+# print(detect_objects('image1.jpg', 0.50))
--- start.py
+++ start.py
@@ -1,42 +1,27 @@
 # coding=UTF-8
-
-import sys
-import os
-
-from flask import Flask, jsonify, request, json, render_template, send_from_directory
+from flask import Flask, jsonify, request
 from flask_api import status
-from flask_cors import CORS, cross_origin
-app = Flask(__name__)
-import requests
 from main import *
-from decimal import Decimal
-
-
 
 app = Flask(__name__)
 
-@app.route('/getTest', methods=["GET"])
-def getTest():
-    return jsonify(result="bla")
 
 @app.route('/getObjects', methods=["POST"])
-def getObjects():
-    #UPLOAD_FOLDER = "/Users/brunocf/Documents/General/CIO/Cognitive-Academy/TensorFlow/object_detection/test_images"
-    UPLOAD_FOLDER = "test_images"
+def get_objects():
+
+    upload_folder = "test_images"
     try:
         imagefile = request.files.get('imagefile')
 
         if imagefile:
-            #filename = secure_filename(imagefile.filename)
-            imagefile.save(os.path.join(UPLOAD_FOLDER, imagefile.filename))
+            imagefile.save(os.path.join(upload_folder, imagefile.filename))
             objs = detect_objects(imagefile.filename, 0.50)
-            os.remove(os.path.join(UPLOAD_FOLDER, imagefile.filename))
-            #print(str(objs))
+            os.remove(os.path.join(upload_folder, imagefile.filename))
             return jsonify(objs)
 
     except Exception as err:
         print(err.args[0])
-        return jsonify(result=err.args[0]),status.HTTP_400_BAD_REQUEST
+        return jsonify(result=err.args[0]), status.HTTP_400_BAD_REQUEST
 
 
 port = int(os.getenv('PORT', 8080))
